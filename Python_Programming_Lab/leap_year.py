bedd6e2ca01c2c3f958f7cfd2ce66ae5587a4217
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-01-17 21:31:32 +0530

    Delete Python_Programming_Lab/leap_year.py

--- Python_Programming_Lab/leap_year.py
+++
@@ -1,5 +0,0 @@
-c_year=int(input("Enter the current year : "))
-f_year=int(input("Enter the final year : "))
-for year in range(c_year,f_year+1):
-    if(year%4==0 and year%100!=0 or year%400==0):
-        print(year)
