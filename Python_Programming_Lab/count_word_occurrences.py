7bf4c68203daec0580212bfcedf8d7a0868e289c
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-01-18 21:46:11 +0530

    Add files via upload

---
+++ Python_Programming_Lab/count_word_occurrences.py
@@ -0,0 +1,10 @@
+text=input("Enter a line of text : ")
+words=text.split()
+word_count={}
+for word in words:
+    if word in word_count:
+        word_count[word]+=1
+    else:
+        word_count[word]=1
+for word,count in word_count.items():
+    print(word,count)
