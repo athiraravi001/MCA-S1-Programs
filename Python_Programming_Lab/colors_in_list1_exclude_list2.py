a888eef8e4abf0bbc7ff9827d9090f6b4f2e67f2
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-01-18 22:05:42 +0530

    Add files via upload

---
+++ Python_Programming_Lab/colors_in_list1_exclude_list2.py
@@ -0,0 +1,5 @@
+color_l1=["red","green","white","black","pink"]
+color_l2=["red","green","white"]
+for color in color_l1:
+    if color not in color_l2:
+        print(color)
---
+++ Python_Programming_Lab/compute_(n+nn+nnn).py
@@ -0,0 +1,3 @@
+n=int(input("Enter value for n : "))
+a=n+(n*n)+(n*n*n)
+print("Result : ",a)
