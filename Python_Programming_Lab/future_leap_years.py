37e714fee082b8533ae76a93acd019529b853be7
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-01-18 17:13:30 +0530

    Update future_leap_years.py

--- Python_Programming_Lab/future_leap_years.py
+++ Python_Programming_Lab/future_leap_years.py
@@ -1,5 +1,6 @@
 c_year=int(input("Enter the current year : "))
 f_year=int(input("Enter the final year : "))
+print("Leap years between current year and final year are :")
 for year in range(c_year,f_year+1):
     if(year%4==0 and year%100!=0 or year%400==0):
         print(year)
