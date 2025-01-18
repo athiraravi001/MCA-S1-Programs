478d5f10d98dfb2eb8b1fe43a95e1d9efdcbebd7
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-01-18 15:53:57 +0530

    Add files via upload

---
+++ Python_Programming_Lab/Modules_and_Packages/Built-in_Packages/matplotlib/scatter_diagram.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+x=[5,7,10]
+y=[18,10,6]
+x2=[3,9,12]
+y2=[20,13,8]
+plt.title('Scatter Diagram')
+plt.scatter(x,y,color='r')
+plt.scatter(x2,y2,color='g')
+plt.show()
