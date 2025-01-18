8a032dd7cc037968df5a8ce2fa0ab900c15cd713
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-01-19 00:35:54 +0530

    Update dict_to_csv.py

--- Python_Programming_Lab/Files/dict_to_csv.py
+++ Python_Programming_Lab/Files/dict_to_csv.py
@@ -1,5 +1,5 @@
 import csv
-cars=cars=[{'No':1,'Company':'Ferrari','Model':'488GTB'},{'No':2,'Company':'BMW','Model':'BMWX7'},{'No':3,'Company':'Porsche','Model':'918spyder'}]
+cars=[{'No':1,'Company':'Ferrari','Model':'488GTB'},{'No':2,'Company':'BMW','Model':'BMWX7'},{'No':3,'Company':'Porsche','Model':'918spyder'}]
 csvfile=open('Car.csv','w')
 field_names=list(cars[0].keys())
 writer=csv.DictWriter(csvfile,fieldnames=field_names)
