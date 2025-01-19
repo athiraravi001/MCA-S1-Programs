61bbab8cef794279d523064568b495200c70dade
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-01-19 21:42:04 +0530

    Update swap_first_last_char.py

--- Python_Programming_Lab/swap_first_last_char.py
+++ Python_Programming_Lab/swap_first_last_char.py
@@ -1,6 +1,6 @@
 str = input("Enter a string : ")
-first_char = str[0]
-last_char = str[-1]
+first_char = str[0]
 middle_part = str[1:-1]
+last_char = str[-1]
 new_str = last_char + middle_part + first_char
 print("New string : ", new_str)
